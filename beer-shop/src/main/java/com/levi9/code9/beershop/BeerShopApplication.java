52626df878c2fb1362c59b565130fc560d41341d
GrujicDejan <dejan.grujic2000@gmail.com>
2022-05-18 17:27:19 +0200

    initial commit

---
+++ beer-shop/src/main/java/com/levi9/code9/beershop/BeerShopApplication.java
@@ -0,0 +1,13 @@
+package com.levi9.code9.beershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerShopApplication.class, args);
+	}
+
+}
